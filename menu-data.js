49782f90a0e5f5e0e25331c2fefd73bf447cc0b8
Yaser Al-Halabi <yaseralhalabi28@gmail.com>
2026-08-06 18:41:41 +0300

    Improve SEO

--- menu-data.js
+++ menu-data.js
@@ -145,7 +145,7 @@ window.MENU = {
     { cat: "meals", sub: "jumbo", name: "وجبة جامبو كريسبي", ingredients: "10 قطع كريسبي - اكسترا بطاطا - مايونيز - كول سلو ", price: 1000, img: "/images/sandwich-meat.webp" },
     { cat: "meals", sub: "jumbo", name: "وجبة جامبو اسكالوب", ingredients: "10 قطع اسكالوب - اكسترا بطاطا - مايونيز - كول سلو ", price: 1000, img: "/images/sandwich-meat.webp" },
     { cat: "meals", sub: "jumbo", name: "وجبة جامبو زنجر", ingredients: "10 قطع زنجر(سبايسي - ذرة) - اكسترا بطاطا - مايونيز - كول سلو ", price: 1000, img: "/images/sandwich-meat.webp" },
-    { cat: "meals", sub: "jumbo", name: "وجبة جامبو سوبربم ميني", ingredients: "8 قطع سوبريم ميني - اكسترا بطاطا - مايونيز - كول سلو ", price: 1000, img: "/images/sandwich-meat.webp" },
+    { cat: "meals", sub: "jumbo", name: "وجبة جامبو سوبريم ميني", ingredients: "8 قطع سوبريم ميني - اكسترا بطاطا - مايونيز - كول سلو ", price: 1000, img: "/images/sandwich-meat.webp" },
 
     /* ---------- سلطات ---------- */
     { cat: "salads", name: "سلطة سيزر ", ingredients: "خس - صوص سيزر - دجاج - ميني توست - جبنة موزريلا", price: 400, img: "/images/sandwich-meat.webp" },
